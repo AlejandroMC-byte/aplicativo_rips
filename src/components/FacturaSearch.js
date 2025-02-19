3e7f0110c11bad847e7f4af52f671b020dc74761
Alejandro Montero Cardona <alejojunior0510@gmail.com>
2025-02-18 23:17:28 -0500

    Se agrega el envio por pagina y ajustes al buscador

--- src/components/FacturaSearch.js
+++ src/components/FacturaSearch.js
@@ -1,18 +1,32 @@
 import { useState } from "react";
 import BotonesJson from "./BotonesJson";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 function BuscadorFacturas() {
   const [facturas, setFacturas] = useState([]);
   const [filtros, setFiltros] = useState({ prefijo: "", factura_fiscal: "", fecha_registro: "" });
   const [mensaje, setMensaje] = useState("");
   const [buscandoFacturas, setBuscandoFacturas] = useState(false);
+  const [enviandopagina, setEnviandopagina] = useState(false);
+
   const buscarFacturas = async () => {
+    if ((filtros.prefijo && !filtros.factura_fiscal) || (!filtros.prefijo && filtros.factura_fiscal)) {
+      toast.error("Debe llenar ambos campos: Prefijo y Factura Fiscal", { position: "top-center" });
+      return;
+    }
+
+    if((filtros.prefijo && filtros.fecha_registro) || (filtros.factura_fiscal && filtros.fecha_registro) || (filtros.prefijo && filtros.factura_fiscal && filtros.fecha_registro)){
+      toast.error("Si llenas la fecha no llenes el prefijo o factura fiscal", { position: "top-center" });
+      return;
+    }
+
     const requestBody = { ...filtros, consultarFacturas: '1' };
-    const toastId = toast.loading("Buscando facturas...");
-  
+    const toastId = toast.loading("Buscando facturas...", { position: "top-center" });
+
     try {
       setBuscandoFacturas(true); // Bloquea el buscador
-  
+
       const response = await fetch(`http://172.16.0.117/SIIS_DIME/webservices/ApiFacturasRipsElectronicos/`, {
         method: 'POST',
         headers: {
@@ -20,22 +34,22 @@ function BuscadorFacturas() {
         },
         body: JSON.stringify(requestBody)
       });
-  
+
       const data = await response.json();
-      console.log("Response Data:", data); // Depuración
-  
+      // console.log("Response Data:", data); // Depuración
+
       setFacturas(Array.isArray(data.facturas) ? data.facturas : []);
       setMensaje(data.message || "");
-  
+
       const esError = data.message?.includes("error") || !data.facturas?.length;
-  
+
       toast.update(toastId, {
         render: data.message || "Facturas cargadas correctamente",
         type: esError ? "error" : "success",
         isLoading: false,
         autoClose: 2000
       });
-  
+
     } catch (error) {
       console.error("Error al buscar facturas:", error);
       toast.update(toastId, {
@@ -48,10 +62,11 @@ function BuscadorFacturas() {
       setBuscandoFacturas(false); // Desbloquea el buscador
     }
   };
+
   const enviarRIPSfactura = async (factura) => {
     const requestBody = { ...factura, envioRips: '1' };
     const toastId = toast.loading("Enviando RIPS electrónicos...");
-  
+
     try {
       const response = await fetch(`http://172.16.0.117/SIIS_DIME/webservices/ApiFacturasRipsElectronicos/`, {
         method: 'POST',
@@ -60,67 +75,101 @@ function BuscadorFacturas() {
         },
         body: JSON.stringify(requestBody)
       });
-  
+
       const data = await response.json();
       const mensaje = data.message || "Respuesta no esperada";
-  
+
       // Verificamos si el mensaje indica un error
       const esError = mensaje.includes("contiene errores");
-  
-      toast.update(toastId, { 
-        render: mensaje, 
-        type: esError ? "error" : "success", 
-        isLoading: false, 
-        autoClose: 2000 
+
+      toast.update(toastId, {
+        render: mensaje,
+        type: esError ? "error" : "success",
+        isLoading: false,
+        autoClose: 2000
       });
-  
+
       setMensaje(mensaje);
-  
+
       // Si el mensaje confirma el envío exitoso, eliminamos la factura de la lista
       if (mensaje.includes(`Documento ${factura.prefijo}-${factura.factura_fiscal} de la cuenta ${factura.numerodecuenta} fue enviada correctamente.`)) {
-        setFacturas(prevFacturas => prevFacturas.filter(f => 
+        setFacturas(prevFacturas => prevFacturas.filter(f =>
           !(f.prefijo === factura.prefijo && f.factura_fiscal === factura.factura_fiscal && f.numerodecuenta === factura.numerodecuenta)
         ));
       }
-      
+
     } catch (error) {
-      toast.update(toastId, { 
-        render: "Error al conectar con el servidor", 
-        type: "error", 
-        isLoading: false, 
-        autoClose: 5000 
+      toast.update(toastId, {
+        render: "Error al conectar con el servidor",
+        type: "error",
+        isLoading: false,
+        autoClose: 5000
       });
     }
   };
+
+  const envioFacturasPagina = async () => {
+    setEnviandopagina(true); // Bloquea el buscador
+    const toastId = toast.loading("Enviando todas las facturas...", { position: "top-center" });
+    try {
+      for (const factura of facturas) {
+        await enviarRIPSfactura(factura);
+      }
+      toast.update(toastId, {
+        render: "Todas las facturas han sido enviadas correctamente",
+        type: "success",
+        isLoading: false,
+        autoClose: 2000
+      });
+    } catch (error) {
+      toast.update(toastId, {
+        render: "Error al enviar las facturas",
+        type: "error",
+        isLoading: false,
+        autoClose: 5000
+      });
+    } finally {
+      setEnviandopagina(false); // Desbloquea el buscador
+    }
+  };
+  const limpiarFormulario = () => {
+    setFiltros({ prefijo: "", factura_fiscal: "", fecha_registro: "" });
+    setMensaje("");
+    setFacturas([]);
+  };
+
   const formatearComoPesos = (valor) => {
     // Convertir el valor a número
     const numero = Number(valor);
-    
+
     // Si el valor es inválido, devolver "$ 0"
     if (isNaN(numero)) return "$ 0";
-  
+
     // Formatear correctamente
     return new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(numero);
   };
-  
 
   return (
     <div className="container mt-4">
+      <ToastContainer />
       <h2 className="mb-3">Buscar Facturas para RIPS electronicos</h2>
       <div className="row">
         <div className="col-md-3">
-          <input className="form-control" type="text" placeholder="Prefijo" onChange={(e) => setFiltros({ ...filtros, prefijo: e.target.value })} />
+          <input className="form-control" type="text" placeholder="Prefijo" value={filtros.prefijo} onChange={(e) => setFiltros({ ...filtros, prefijo: e.target.value })} />
         </div>
         <div className="col-md-3">
-          <input className="form-control" type="text" placeholder="Factura Fiscal" onChange={(e) => setFiltros({ ...filtros, factura_fiscal: e.target.value })} />
+          <input className="form-control" type="text" placeholder="Factura Fiscal" value={filtros.factura_fiscal} onChange={(e) => setFiltros({ ...filtros, factura_fiscal: e.target.value })} />
         </div>
         <div className="col-md-3">
-          <input className="form-control" type="date" onChange={(e) => setFiltros({ ...filtros, fecha_registro: e.target.value })} />
+          <input className="form-control" type="date" value={filtros.fecha_registro} onChange={(e) => setFiltros({ ...filtros, fecha_registro: e.target.value })} />
         </div>
-        <div className="col-md-3">
-        <button className="btn btn-primary" onClick={buscarFacturas} disabled={buscandoFacturas}>
-          {buscandoFacturas ? "Buscando..." : "Buscar Facturas"}
-        </button>
+        <div className="col-md-3 d-flex">
+          <button className="btn btn-primary me-2" onClick={buscarFacturas} disabled={buscandoFacturas}>
+            {buscandoFacturas ? "Buscando..." : "Buscar Facturas"}
+          </button>
+          <button className="btn btn-secondary" onClick={limpiarFormulario} disabled={buscandoFacturas}>
+            Limpiar
+          </button>
         </div>
       </div>
       {mensaje && (
@@ -130,53 +179,60 @@ function BuscadorFacturas() {
       )}
 
       {facturas.length > 0 && (
-        <table className="table mt-3">
-          <thead>
-            <tr>
-              <th style={{ textAlign: "center" }}>Prefijo</th>
-              <th style={{ textAlign: "center" }}>Factura Fiscal</th>
-              <th style={{ textAlign: "center" }}>Cuenta</th>
-              <th style={{ textAlign: "center" }}>Estado factura SIIS</th>
-              <th style={{ textAlign: "center" }}>Total Factura</th>
-              <th style={{ textAlign: "center" }}>Fecha Registro</th>
-              <th style={{ textAlign: "center" }}>Estado Factura Electronica</th>
-              <th style={{ textAlign: "center" }}>Estado RIPS Electronico</th>
-              <th style={{ textAlign: "center" }}>JSON RIPS</th>
-              <th style={{ textAlign: "center" }}>Envio rips</th>
-            </tr>
-          </thead>
-          <tbody>
-            {facturas.map((factura) => (
-              <tr key={factura.factura_fiscal}>
-                <td style={{ textAlign: "center" }}>{factura.prefijo}</td>
-                <td style={{ textAlign: "center" }}>{factura.factura_fiscal}</td>
-                <td style={{ textAlign: "center" }}>{factura.numerodecuenta}</td>
-                <td style={{ textAlign: "center" }}>{factura.estado}</td>
-                <td style={{ textAlign: "center" }}>
-                  {formatearComoPesos(factura.total_factura)}
-                </td>
-                <td style={{ textAlign: "center" }}>{factura.fecha_registro}</td>
-                <td style={{ textAlign: "center" }}>{factura.estado_fac_electronica}</td>
-                <td style={{ textAlign: "center" }}>{factura.estado_rips}</td>
-                <td>
-                  <BotonesJson jsonRespuesta={factura.json_respuesta} jsonRips={factura.json_rips} />
-                </td>
-                <td>
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => enviarRIPSfactura({
-                      prefijo: factura.prefijo,
-                      factura_fiscal: factura.factura_fiscal,
-                      numerodecuenta: factura.numerodecuenta
-                    })}
-                  >
-                    Enviar RIPS
-                  </button>
-                </td>
+        <>
+          <div className="col-md-3">
+            <button className="btn btn-primary" onClick={envioFacturasPagina} disabled={enviandopagina}>
+              {enviandopagina ? "Enviando Facturas pagina..." : "Enviar facturas del dia"}
+            </button>
+          </div>
+          <table className="table mt-3">
+            <thead>
+              <tr>
+                <th style={{ textAlign: "center" }}>Prefijo</th>
+                <th style={{ textAlign: "center" }}>Factura Fiscal</th>
+                <th style={{ textAlign: "center" }}>Cuenta</th>
+                <th style={{ textAlign: "center" }}>Estado factura SIIS</th>
+                <th style={{ textAlign: "center" }}>Total Factura</th>
+                <th style={{ textAlign: "center" }}>Fecha Registro</th>
+                <th style={{ textAlign: "center" }}>Estado Factura Electronica</th>
+                <th style={{ textAlign: "center" }}>Estado RIPS Electronico</th>
+                <th style={{ textAlign: "center" }}>JSON RIPS</th>
+                <th style={{ textAlign: "center" }}>Envio rips</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {facturas.map((factura) => (
+                <tr key={factura.factura_fiscal}>
+                  <td style={{ textAlign: "center" }}>{factura.prefijo}</td>
+                  <td style={{ textAlign: "center" }}>{factura.factura_fiscal}</td>
+                  <td style={{ textAlign: "center" }}>{factura.numerodecuenta}</td>
+                  <td style={{ textAlign: "center" }}>{factura.estado}</td>
+                  <td style={{ textAlign: "center" }}>
+                    {formatearComoPesos(factura.total_factura)}
+                  </td>
+                  <td style={{ textAlign: "center" }}>{factura.fecha_registro}</td>
+                  <td style={{ textAlign: "center" }}>{factura.estado_fac_electronica}</td>
+                  <td style={{ textAlign: "center" }}>{factura.estado_rips}</td>
+                  <td>
+                    <BotonesJson jsonRespuesta={factura.json_respuesta} jsonRips={factura.json_rips} />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => enviarRIPSfactura({
+                        prefijo: factura.prefijo,
+                        factura_fiscal: factura.factura_fiscal,
+                        numerodecuenta: factura.numerodecuenta
+                      })}
+                    >
+                      Enviar RIPS
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       )}
     </div>
   );
