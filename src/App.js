b5f5f48a899e943bdcc100829a886ed049536110
AlejandroMC-byte <alejojunior0510@gmail.com>
2025-03-24 22:01:26 -0500

    Se agrega el SideBar y se agregan stilos personalizados

--- src/App.js
+++ src/App.js
@@ -4,23 +4,39 @@ import RipsFacturaSearch from "./components/RipsFacturaSearch";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Header from "./components/Header"; // Cambiado de Footer a Header
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar"; // Importar el Sidebar
 
-// Componente temporal para FacturaSearch
+// Componentes temporales para los submódulos
 const FacturaSearch = () => <div className="p-4">FacturaSearch en construcción...</div>;
+const NotasCredito = () => <div className="p-4">Notas Crédito en construcción...</div>;
+const NotasDebito = () => <div className="p-4">Notas Débito en construcción...</div>;
+const NotasCreditoGlosas = () => <div className="p-4">Notas Crédito Glosas en construcción...</div>;
+const RipsNotasCreditoGlosas = () => <div className="p-4">RIPS Notas Crédito Glosas en construcción...</div>;
+const ReporteFacturacion = () => <div className="p-4">Reporte Facturación en construcción...</div>;
+const ReporteRipsElectronicos = () => <div className="p-4">Reporte RIPS Electrónicos en construcción...</div>;
 
 function App() {
   return (
-    <Router> {/* Router solo aquí */}
+    <Router>
       <div className="App d-flex flex-column min-vh-100">
-        <Header /> {/* Agregamos el Header */}
+        <Header />
         <ToastContainer />
-        <div className="flex-grow-1 p-4">
-          <Routes>
-            <Route path="/rips-factura" element={<RipsFacturaSearch />} />
-            <Route path="/factura" element={<FacturaSearch />} />
-            <Route path="*" element={<RipsFacturaSearch />} /> {/* Vista por defecto */}
-          </Routes>
+        <div className="d-flex flex-grow-1">
+          <Sidebar /> {/* Agregar el Sidebar */}
+          <div className="flex-grow-1 p-4">
+            <Routes>
+              <Route path="aplicativo_rips/build/rips-factura" element={<RipsFacturaSearch />} />
+              <Route path="aplicativo_rips/build/factura" element={<FacturaSearch />} />
+              <Route path="aplicativo_rips/build/notas-credito" element={<NotasCredito />} />
+              <Route path="aplicativo_rips/build/notas-debito" element={<NotasDebito />} />
+              <Route path="aplicativo_rips/build/notas-credito-glosas" element={<NotasCreditoGlosas />} />
+              <Route path="aplicativo_rips/build/rips-notas-credito-glosas" element={<RipsNotasCreditoGlosas />} />
+              <Route path="aplicativo_rips/build/reporte-facturacion" element={<ReporteFacturacion />} />
+              <Route path="aplicativo_rips/build/reporte-rips-electronicos" element={<ReporteRipsElectronicos />} />
+              <Route path="*" element={<RipsFacturaSearch />} /> {/* Vista por defecto */}
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Styles/Header.css"; // Importa el archivo CSS personalizado
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg header-custom">
       <div className="container">
         <Link className="navbar-brand fw-bold" to="/rips-factura">
           Buscador de Facturas
@@ -11,12 +12,12 @@ const Header = () => {
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/rips-factura">
+              <Link className="nav-link" to="/rips-factura">
                 Rips Factura
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/factura">
+              <Link className="nav-link" to="/factura">
                 Factura Electronica
               </Link>
             </li>
